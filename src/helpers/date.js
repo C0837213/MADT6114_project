489046c763c5077f1522561d2056222ee5914d77
mark chan <mark_chan74@hotmail.com>
2022-11-14 13:55:33 -0500

    ✅ complete admin home

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -3,3 +3,41 @@ import moment from "moment";
 export const getDateString = (date) => {
   return moment(date, "x").format("DD MMM YYYY hh:mm a");
 };
+
+export const filterByTime = (arr, type) => {
+  const list = [...arr];
+  let result;
+  if (type !== "all") {
+    if (type === "lastWeek") {
+      result = list.filter((item) => {
+        const beginDateTime = moment()
+          .subtract(1, "weeks")
+          .startOf("week")
+          .unix();
+        const endDateTime = moment().subtract(1, "weeks").endOf("week").unix();
+        const itemCreateTime = moment(item.createdAt, "x").valueOf();
+        if (itemCreateTime > beginDateTime && itemCreateTime < endDateTime) {
+          return item;
+        }
+      });
+    } else if (type === "lastMonth") {
+      result = list.filter((item) => {
+        const beginDateTime = moment()
+          .subtract(1, "months")
+          .startOf("months")
+          .unix();
+        const endDateTime = moment()
+          .subtract(1, "months")
+          .endOf("months")
+          .unix();
+        const itemCreateTime = moment(item.createdAt, "x").valueOf();
+        if (itemCreateTime > beginDateTime && itemCreateTime < endDateTime) {
+          return item;
+        }
+      });
+    }
+  } else {
+    result = list;
+  }
+  return result;
+};
--- src/screens/AdminHome.js
+++ src/screens/AdminHome.js
@@ -3,119 +3,111 @@ import React from "react";
 import { getAllOrders } from "../services/firebase";
 import { VStack, Text, HStack, Button } from "native-base";
 import moment from "moment";
+import { filterByTime } from "../helpers/date";
 
-const dummy = [
-  {
-    catId: "k5rcC9QYgLXnB6pUCbkd",
-    catName: "Meat",
-    createdAt: "1667882967",
-    id: "Ee6GfDO1WEkl0joCTgI3",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406137036png?alt=media&token=dc73e978-1500-418e-9350-fb4f0da07d0d",
-    name: "Meat1",
-    price: "25",
-    quantity: 1,
-  },
-  {
-    catId: "Q0Kn1V3y28G5XjZXCxJB",
-    catName: "Drink",
-    createdAt: "1667882967",
-    id: "YwtGEpiLQ6tG8tLzHtKU",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406540796png?alt=media&token=915c36c2-daf2-484b-aa6f-a5f239002f08",
-    name: "Coke",
-    price: "1",
-    quantity: 2,
-  },
-  {
-    catId: "Q0Kn1V3y28G5XjZXCxJB",
-    catName: "Drink",
-    createdAt: "1667882967",
-    id: "ju2Kc38Xl51EghHWpSIy",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406564465jpg?alt=media&token=7e3119c0-622e-4811-af2a-9438e5446c33",
-    name: "Water",
-    price: "1",
-    quantity: 3,
-  },
-  {
-    catId: "k5rcC9QYgLXnB6pUCbkd",
-    catName: "Meat",
-    createdAt: "1662868167",
-    id: "Ee6GfDO1WEkl0joCTgI3",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406137036png?alt=media&token=dc73e978-1500-418e-9350-fb4f0da07d0d",
-    name: "Meat1",
-    price: "25",
-    quantity: 1,
-  },
-  {
-    catId: "k5rcC9QYgLXnB6pUCbkd",
-    catName: "Meat",
-    createdAt: "1665460167",
-    id: "Ee6GfDO1WEkl0joCTgI3",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406137036png?alt=media&token=dc73e978-1500-418e-9350-fb4f0da07d0d",
-    name: "Meat1",
-    price: "25",
-    quantity: 2,
-  },
-];
+// const dummy = [
+//   {
+//     catId: "k5rcC9QYgLXnB6pUCbkd",
+//     catName: "Meat",
+//     createdAt: "1667882967",
+//     id: "Ee6GfDO1WEkl0joCTgI3",
+//     image:
+//       "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406137036png?alt=media&token=dc73e978-1500-418e-9350-fb4f0da07d0d",
+//     name: "Meat1",
+//     price: "25",
+//     quantity: 1,
+//   },
+//   {
+//     catId: "Q0Kn1V3y28G5XjZXCxJB",
+//     catName: "Drink",
+//     createdAt: "1667882967",
+//     id: "YwtGEpiLQ6tG8tLzHtKU",
+//     image:
+//       "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406540796png?alt=media&token=915c36c2-daf2-484b-aa6f-a5f239002f08",
+//     name: "Coke",
+//     price: "1",
+//     quantity: 2,
+//   },
+//   {
+//     catId: "Q0Kn1V3y28G5XjZXCxJB",
+//     catName: "Drink",
+//     createdAt: "1667882967",
+//     id: "ju2Kc38Xl51EghHWpSIy",
+//     image:
+//       "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406564465jpg?alt=media&token=7e3119c0-622e-4811-af2a-9438e5446c33",
+//     name: "Water",
+//     price: "1",
+//     quantity: 3,
+//   },
+//   {
+//     catId: "k5rcC9QYgLXnB6pUCbkd",
+//     catName: "Meat",
+//     createdAt: "1662868167",
+//     id: "Ee6GfDO1WEkl0joCTgI3",
+//     image:
+//       "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406137036png?alt=media&token=dc73e978-1500-418e-9350-fb4f0da07d0d",
+//     name: "Meat1",
+//     price: "25",
+//     quantity: 1,
+//   },
+//   {
+//     catId: "k5rcC9QYgLXnB6pUCbkd",
+//     catName: "Meat",
+//     createdAt: "1665460167",
+//     id: "Ee6GfDO1WEkl0joCTgI3",
+//     image:
+//       "https://firebasestorage.googleapis.com/v0/b/mad6114-2.appspot.com/o/1668406137036png?alt=media&token=dc73e978-1500-418e-9350-fb4f0da07d0d",
+//     name: "Meat1",
+//     price: "25",
+//     quantity: 2,
+//   },
+// ];
 
 const AdminHome = () => {
   const [orders, setOrders] = React.useState([]);
   const [ordersByCat, setOrderByCat] = React.useState([]);
   const [orderByProd, setOrderByProd] = React.useState([]);
   const [catFilterType, setCatFilterType] = React.useState("all");
+  const [proFilterType, setProFilterType] = React.useState("all");
 
   React.useEffect(() => {
-    // fetchAllOrders();
+    fetchAllOrders();
     countByCat("all");
+    countByProduct("all");
   }, []);
 
   React.useEffect(() => {
     countByCat(catFilterType);
   }, [catFilterType]);
 
-  const countByCat = (type) => {
-    const list = [...dummy];
+  React.useEffect(() => {
+    countByProduct(proFilterType);
+  }, [proFilterType]);
+
+  const countByProduct = (type) => {
+    const list = [...orders];
     const resultArr = [];
-    let result;
-    if (type !== "all") {
-      if (type === "lastWeek") {
-        result = list.filter((item) => {
-          const beginDateTime = moment()
-            .subtract(1, "weeks")
-            .startOf("week")
-            .unix();
-          const endDateTime = moment()
-            .subtract(1, "weeks")
-            .endOf("week")
-            .unix();
-          const itemCreateTime = moment(item.createdAt, "x").valueOf();
-          if (itemCreateTime > beginDateTime && itemCreateTime < endDateTime) {
-            return item;
-          }
-        });
-      } else if (type === "lastMonth") {
-        result = list.filter((item) => {
-          const beginDateTime = moment()
-            .subtract(1, "months")
-            .startOf("months")
-            .unix();
-          const endDateTime = moment()
-            .subtract(1, "months")
-            .endOf("months")
-            .unix();
-          const itemCreateTime = moment(item.createdAt, "x").valueOf();
-          if (itemCreateTime > beginDateTime && itemCreateTime < endDateTime) {
-            return item;
-          }
-        });
-      }
-    } else {
-      result = list;
+    let result = filterByTime(list, type);
+    if (result.length > 0) {
+      result = result.reduce((acc, curItem) => {
+        if (!acc[curItem.name]) {
+          acc[curItem.name] = 1 * parseInt(curItem.quantity);
+        } else {
+          acc[curItem.name] = acc[curItem.name] + parseInt(curItem.quantity);
+        }
+        return acc;
+      }, {});
+      Object.keys(result).forEach((key) => {
+        resultArr.push({ prodName: key, number: result[key] });
+      });
+      setOrderByProd(resultArr);
     }
+  };
+
+  const countByCat = (type) => {
+    const list = [...orders];
+    const resultArr = [];
+    let result = filterByTime(list, type);
     if (result.length > 0) {
       result = result.reduce((acc, curItem) => {
         if (!acc[curItem.catName]) {
@@ -150,38 +142,63 @@ const AdminHome = () => {
 
   return (
     <View style={styles.container}>
-      <Text fontSize={"lg"}>Sales In Categories</Text>
       <FlatList
-        style={{ width: "90%", paddingVertical: 16 }}
-        ListHeaderComponent={() => {
-          return (
+        ListHeaderComponent={() => (
+          <Text fontSize={"lg"}>Sales In Categories</Text>
+        )}
+        data={ordersByCat}
+        ListFooterComponent={() => (
+          <>
+            <HStack space={4} justifyContent={"center"}>
+              <Button onPress={() => setCatFilterType("lastWeek")}>
+                Last week
+              </Button>
+              <Button onPress={() => setCatFilterType("lastMonth")}>
+                Last month
+              </Button>
+              <Button onPress={() => setCatFilterType("all")}>All time</Button>
+            </HStack>
             <FlatList
-              data={ordersByCat}
-              ListFooterComponent={() => (
-                <HStack space={4} justifyContent={"center"}>
-                  <Button onPress={() => setCatFilterType("lastWeek")}>
-                    Last week
-                  </Button>
-                  <Button onPress={() => setCatFilterType("lastMonth")}>
-                    Last month
-                  </Button>
-                  <Button onPress={() => setCatFilterType("all")}>
-                    All time
-                  </Button>
-                </HStack>
+              data={orderByProd}
+              ListHeaderComponent={() => (
+                <Text fontSize={"lg"}>Sales In Product</Text>
               )}
               ItemSeparatorComponent={() => (
                 <View style={{ height: 1.5, backgroundColor: "lightgray" }} />
               )}
               renderItem={({ item }) => (
                 <VStack space={4} marginTop={4} marginBottom={4}>
-                  <Text>Categories name: {item.catName}</Text>
+                  <Text>Product name: {item.prodName}</Text>
                   <Text>Number of sales: {item.number}</Text>
                 </VStack>
               )}
+              style={{ width: "90%", paddingVertical: 16 }}
+              ListFooterComponent={() => (
+                <HStack space={4} justifyContent={"center"}>
+                  <Button onPress={() => setProFilterType("lastWeek")}>
+                    Last week
+                  </Button>
+                  <Button onPress={() => setProFilterType("lastMonth")}>
+                    Last month
+                  </Button>
+                  <Button onPress={() => setProFilterType("all")}>
+                    All time
+                  </Button>
+                </HStack>
+              )}
             />
-          );
-        }}
+          </>
+        )}
+        style={{ width: "90%", paddingVertical: 16 }}
+        ItemSeparatorComponent={() => (
+          <View style={{ height: 1.5, backgroundColor: "lightgray" }} />
+        )}
+        renderItem={({ item }) => (
+          <VStack space={4} marginTop={4} marginBottom={4}>
+            <Text>Categories name: {item.catName}</Text>
+            <Text>Number of sales: {item.number}</Text>
+          </VStack>
+        )}
       />
     </View>
   );
