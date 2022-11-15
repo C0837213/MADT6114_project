4e3d2ba1f95b825f6a86efe31907cb2541db9977
mark chan <mark_chan74@hotmail.com>
2022-11-14 20:05:41 -0500

    ✅ update stat

--- src/screens/AdminHome.js
+++ src/screens/AdminHome.js
@@ -133,16 +133,14 @@ const AdminHome = () => {
     if (orders.length === 0) {
       const res = await getAllOrders();
       const orderItems = [];
-      res
-        .filter((item) => item.status !== "pending")
-        .forEach((item) => {
-          if (item.items.length > 0) {
-            item.items.forEach((_item) => {
-              const product = { ..._item, createdAt: item.createdAt };
-              orderItems.push(product);
-            });
-          }
-        });
+      res.forEach((item) => {
+        if (item.items.length > 0) {
+          item.items.forEach((_item) => {
+            const product = { ..._item, createdAt: item.createdAt };
+            orderItems.push(product);
+          });
+        }
+      });
 
       setOrders(orderItems);
     }
